4fbed94d372c5b78d11b4092e352a3cd432d7b5c
Adriana <adrianajaworska1717@gmail.com>
2026-08-13 13:17:49 +0200

    test.sh updated also added commnets for my group memeber s for eeasier review

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Testing application..."
+
+# delay the job a bit
+ wait for services to start
+forsleep 5
+
+# Test frontend
+echo "Testing frontend..."
+# send request to server
+
+curl -f http://localhost:8080/healthz
+if [ $? -eq 0 ]; then
+  #cool, working
+  echo "Frontend working"
+else
+  #didnt respond
+  echo "Frontend failed"
+  # exit 1 = stop
+  exit 1
+fi
+
+# Test backend
+echo "Testing backend..."
+curl -f http://localhost:9000/fortunes/random
+# Check if backend responded
+if [ $? -eq 0 ]; then
+  # success
+  echo "Backend working"
+else
+  #didnt work
+  echo "Backend failed"
+  exit 1
+fi
+
+echo "CONGRATSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
