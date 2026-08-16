878c2221cf8065917ce3970c125e2b362f5bc9ac
Salar778 <salar.ahmed5277@gmail.com>
2026-08-16 08:06:06 +0000

    Implemented ingress and configmap and resolved issues in it

--- deploy.sh
+++ deploy.sh
@@ -18,18 +18,26 @@ fi
 
 echo "Kubernetes namespace: $NAMESPACE"
 
-# Apply frontend deployment
-echo "Deploying frontend..."
-kubectl --kubeconfig kubeconfig apply -f kubernetes/deployment-frontend.yaml
-kubectl --kubeconfig kubeconfig apply -f kubernetes/service-frontend.yaml
+#Apply Database
+kubectl --kubeconfig kubeconfig apply -f kubernetes/configmap.yaml
+kubectl --kubeconfig kubeconfig apply -f kubernetes/persistant-volume.yaml
+kubectl --kubeconfig kubeconfig apply -f kubernetes/deployment-redis.yaml
+kubectl --kubeconfig kubeconfig apply -f kubernetes/service-redis.yaml
 
 # Apply backend deployment
 echo "Deploying backend..."
 kubectl --kubeconfig kubeconfig apply -f kubernetes/deployment-backend.yaml
 kubectl --kubeconfig kubeconfig apply -f kubernetes/service-backend.yaml
 
+# Apply frontend deployment
+echo "Deploying frontend..."
+kubectl --kubeconfig kubeconfig apply -f kubernetes/deployment-frontend.yaml
+kubectl --kubeconfig kubeconfig apply -f kubernetes/service-frontend.yaml
+kubectl --kubeconfig kubeconfig apply -f kubernetes/ingress.yaml
+
 kubectl --kubeconfig kubeconfig get pods -n "$NAMESPACE"
 kubectl --kubeconfig kubeconfig get svc -n "$NAMESPACE"
+kubectl --kubeconfig kubeconfig get ingress -n "$NAMESPACE"
 
 echo "done"
 
